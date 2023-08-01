7e7f33946f530b94a0d2aed1110a4ec3bb9a666b
Ricardo Oliveira <r.deoliveira527@gmail.com>
2023-08-01 19:32:24 +0100

    added questions to quiz

---
+++ assets/js/questions.js
@@ -0,0 +1,31 @@
+// Define an array of quiz questions and choices
+const questions = [
+    {
+      question: 'What does HTML stand for?',
+      choices: ['Hyper Tag Markup Language', 'Hyper Text Markup Language',
+        'Hyperlinks Text Mark Language', 'Hyperlinking Text Marking Language'],
+      correctIndex: 1, // Index of the correct answer in the choices array
+    },
+    {
+        question: 'What symbol indicates a tag?',
+        choices: ['Angle brackets e.g. <>', 'Curved brackets e.g. {}',
+          'Commas e.g. ,', 'Exclamation marks e.g. !'],
+        correctIndex: 0, // Index of the correct answer in the choices array
+    },
+    {
+        question: 'Which of these is a genuine tag keyword?',
+        choices: ['Header', 'Bold', 'Body', 'Image'],
+        correctIndex: 2, // Index of the correct answer in the choices array
+    },
+    {
+        question: 'A CSS file can be applied to only one HTML file.',
+        choices: ['True', 'False'],
+        correctIndex: 1, // Index of the correct answer in the choices array
+    },
+    {
+        question: 'What does CSS stand for?',
+        choices: ['Computing Style Sheet', 'Creative Style System',
+          'Cascading Style Sheet', 'Creative Styling Sheet'],
+        correctIndex: 2, // Index of the correct answer in the choices array
+    },  
+  ];
